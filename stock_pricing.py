ca3551aa3f48c20d97caffc075ecc1db4517bc23
saleh <ghalayinisaleh9@gmail.com>
2024-11-04 19:13:22 +0200

    finished the project and added all the neccesary comments

--- stock_pricing.py
+++ stock_pricing.py
@@ -1,5 +1,24 @@
+#defining a function to calculate the highest profit possible and return it
 def max_profit(prices):
-    print(prices)
-    
 
-max_profit([5,2,7,3,10,9])
+    #assigning the needed variables to do the operation
+    minimum = prices[0]
+    highest_profit = 0
+
+    #in this loop we check to see where the lowest value in the stock is 
+    #and we search for the highest margin value which is the highest profit we will return
+    for i in range(1, len(prices)):
+        if(minimum > prices[i]):
+            minimum = prices[i]
+        elif(highest_profit < prices[i] - minimum ):
+            highest_profit = prices[i] - minimum
+
+    return highest_profit
+
+#assigning the highest profit we will get from the function max_profit to the variable named profit_result
+profit_result = max_profit([5,2,7,3,10,9])
+
+#printing the highest profit obtained from the function
+print(f"the highest profit obtained is {profit_result}")
+
+#i wanted a project with more steps in order to increase the number of pushes but after i finished it i realized that it was too simple for that
